72259a1a3c9b6833d34f4e59ab605593be62d9ab
TonyDev <antoniospano05@outlook.it>
2025-05-21 18:34:42 +0200

    Paolilolium

--- script.js
+++ script.js
@@ -1,8 +1,5 @@
 console.log("ciao")
 
-var navStatus = false;
-var mobileMenu = false;
-var scroll = false;
 var intro = false;
 var pjanoo = new Audio("sounds/pjanoo.mp3");
 
@@ -17,50 +14,6 @@ window.onscroll = () =>
     {
         var bottom = rect.bottom - 750 < window.innerHeight;
     }
-
-    if (bottom)
-    {
-        document.getElementById("fade-in").style.transform = "translateY(20px)";
-        document.getElementById("fade-in").style.opacity = 1;
-    }
-    else
-    {
-        document.getElementById("fade-in").style.transform = "translateY(-20px)";
-        document.getElementById("fade-in").style.opacity = 0;
-    }
-
-    if (!scroll)
-    {
-        scroll = !scroll;
-        document.getElementById("avviso").style.transform = "translateY(0px)";
-    }
-}
-
-function CurtainMenu()
-{
-    document.getElementById("curtain-menu").style.height = !navStatus ? "160px" : "0%";
-    document.getElementById("curtain-menu").style.opacity = !navStatus ? "100%" : "0%";
-    document.getElementById("curtain-menu").style.pointerEvents = !navStatus ? "all" : "none";
-
-    document.getElementById("log-in").style.borderRadius = !navStatus ? "0px" : "15px";
-    
-    navStatus = !navStatus;
-}
-
-function MobileMenuButton()
-{
-    document.getElementById("mobile-curtain-menu-button").style.transform = !mobileMenu ? "rotate(90deg)" : "rotate(0deg)";
-    
-    document.getElementById("mobile-menu-content").style.opacity = !mobileMenu ? "100%" : "0%";
-    document.getElementById("mobile-menu").style.height = !mobileMenu ? "100%" : "0%";
-    document.getElementById("mobile-menu-content").style.pointerEvents = !mobileMenu ? "all" : "none";
-
-    mobileMenu = !mobileMenu;
-}
-
-function RimuoviAvviso()
-{
-    document.getElementById("avviso").style.transform = "translateY(-600px)";
 }
 
 setInterval(() => {
